cf2c08ecc54a94093df53238f5f1dadb842434e0
nuxion <nuxion@gmail.com>
2022-04-20 14:25:18 -0300

    ci/cd: gce provider

--- tests/test_cluster_control.py
+++ tests/test_cluster_control.py
@@ -37,7 +37,7 @@ def test_cluster_control_inventory_machines_by():
 
 def test_inventory_provider():
     i = Inventory()
-    p = i.get_provider("gce")
+    p = i.get_provider("local")
     assert isinstance(p, ProviderSpec)
 
 
