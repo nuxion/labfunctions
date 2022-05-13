c213f61084a7033e6ec86284ce631507f1aab816
nuxion <nuxion@gmail.com>
2022-05-13 18:23:46 -0300

    runcli script

--- scripts/runcli.sh
+++ scripts/runcli.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run --rm -it -e LF_SERVER=yes nuxion/labfunctions:0.8.0-alpha.8 bash
+
+if [ -f ".env.docker" ]; then
+	CMD_PARAMS="--rm -it -e LF_SERVER=yes --env-file=.env.docker nuxion/labfunctions:0.8.0-alpha.8 bash"
+else
+	CMD_PARAMS="--rm -it -e LF_SERVER=yes nuxion/labfunctions:0.8.0-alpha.8 bash"
+fi
+echo "Running docker run ${CMD_PARAMS}"
+docker run $CMD_PARAMS
