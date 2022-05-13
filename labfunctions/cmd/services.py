4e3b23574b4cd9d79783f4d152d950455c0c377f
nuxion <nuxion@gmail.com>
2022-05-13 14:05:44 -0300

    Curve certificates autogenerated when server starts
    - runcli script for a quick cli inside of docker

--- labfunctions/cmd/services.py
+++ labfunctions/cmd/services.py
@@ -1,12 +1,32 @@
 import os
 import sys
+from pathlib import Path
 
 import click
 
 from labfunctions.conf.server_settings import settings
 from labfunctions.control_plane import rqscheduler
 from labfunctions.types.agent import AgentConfig
-from labfunctions.utils import get_external_ip, get_hostname
+from labfunctions.utils import get_external_ip, get_hostname, mkdir_p
+
+from .utils import console
+
+
+def create_secrets_certs():
+    from labfunctions.commands import shell
+
+    if not Path(".secrets/ecsa.priv.pem").is_file():
+        mkdir_p(Path(".secrets").resolve())
+        shell(
+            "openssl ecparam -genkey -name secp521r1 -noout -out .secrets/ecsa.priv.pem"
+        )
+        shell(
+            "openssl ec -in .secrets/ecsa.priv.pem -pubout -out .secrets/ecsa.pub.pem"
+        )
+        console.print("=> Secrets created")
+    else:
+        console.print("=> Keys already exist")
+
 
 hostname = get_hostname()
 
@@ -27,16 +47,26 @@ hostname = get_hostname()
 @click.option(
     "--access-log", "-L", default=False, is_flag=True, help="Enable access_log"
 )
+@click.option(
+    "--init-secrets",
+    "-I",
+    default=False,
+    is_flag=True,
+    help="Create certs for auth system",
+)
 @click.option("--debug", "-D", default=False, is_flag=True, help="Enable Auto reload")
-def webcli(host, port, workers, apps, auto_reload, access_log, debug):
+def webcli(host, port, workers, apps, auto_reload, access_log, debug, init_secrets):
     """Run API Web Server"""
     # pylint: disable=import-outside-toplevel
     from labfunctions.server import create_app
 
+    if init_secrets:
+        create_secrets_certs()
+
     list_bp = apps.split(",")
     app = create_app(settings, list_bp)
     w = int(workers)
-    print("Debug mode: ", debug)
+    console.print("Debug mode: ", debug)
     app.run(
         host=host,
         port=int(port),
---
+++ scripts/runcli.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm -it -e LF_SERVER=yes nuxion/labfunctions:0.8.0-alpha.8 bash
